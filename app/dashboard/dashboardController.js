ca18d1e5e53d60ed8c74b62460f1b47cc3794545
wasilva <contato.wasilva@hotmail.com>
2018-01-18 22:26:08 -0200

    Correção DashboardController.js

--- app/dashboard/dashboardController.js
+++ app/dashboard/dashboardController.js
@@ -1,4 +1,3 @@
-
 (function() {
   angular.module('primeiraApp').controller('DashboardCtrl', [
     '$http',
@@ -9,13 +8,15 @@ function DashboardController($http) {
   const vm = this
   vm.getSummary = function() {
     const url = 'http://localhost:3003/api/billingSummary';
-    $http.get(url).then(function({credit = 0, debt = 0}) {
-      vm.credit = credit
-      vm.debt = debt
-      vm.total = credit - debt
+    $http.get(url).then(function(response) {
+      var data = response.data;
+      vm.credit = data.credit;
+      vm.debt = data.debt;
+      vm.total = data.credit - data.debt;
     })
   }
 
   vm.getSummary()
 }
 })()
+
