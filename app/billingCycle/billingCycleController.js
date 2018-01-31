d566654afa20364c187518a74d986925a2a12c61
wasilva <contato.wasilva@hotmail.com>
2018-01-30 22:33:07 -0200

    Exibição das Abas de Alterar e Excluir

--- app/billingCycle/billingCycleController.js
+++ app/billingCycle/billingCycleController.js
@@ -2,10 +2,11 @@
   angular.module('primeiraApp').controller('BillingCycleCtrl', [
       '$http',
       'msgs',
+      'tabs',
       BillingCycleController
   ])
 
-  function BillingCycleController($http, msgs){
+  function BillingCycleController($http, msgs, tabs){
       const vm = this
       const url = 'http://localhost:3003/api/billingCycles'
 
@@ -13,6 +14,7 @@
           $http.get(url).then(function(response){
               vm.billingCycle = {}
               vm.billingCycles = response.data
+              tabs.show(vm, {tabList: true, tabCreate: true})
           })
       }
 
@@ -25,6 +27,16 @@
         })
       }
 
+      vm.showTabUpdate = function(billingCycle) {
+        vm.billingCycle = billingCycle
+        tabs.show(vm, {tabUpdate: true})
+      }
+    
+      vm.showTabDelete = function(billingCycle) {
+        vm.billingCycle = billingCycle
+        tabs.show(vm, {tabDelete: true})
+      }
+    
       vm.refresh()
   }
 })()
